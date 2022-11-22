4ebc081bb4ffe36a9f2d0c651862fc1435ac74b5
Runinho <3237686+Runinho@users.noreply.github.com>
2022-11-22 09:44:37 +0100

    inference: load category ids from label file

--- stemseg/inference/main.py
+++ stemseg/inference/main.py
@@ -280,28 +280,7 @@ def main(args):
     elif args.dataset == "kittistep":
         sequences, meta_info = parse_generic_video_dataset(KITTISTEPPaths.train_images_dir(), KITTISTEPPaths.val_vds_file())
 
-        # TODO: load categories from label json
-        categories = {0: 'road',
-                      1: 'sidewalk',
-                      2: 'building',
-                      3: 'wall',
-                      4: 'fence',
-                      5: 'pole',
-                      6: 'trafficlight',
-                      7: 'trafficsign',
-                      8: 'vegetation',
-                      9: 'terrain',
-                      10: 'sky',
-                      11: 'person',
-                      12: 'rider',
-                      13: 'car',
-                      14: 'truck',
-                      15: 'bus',
-                      16: 'train',
-                      17: 'motorcycle',
-                      18: 'bicycle',
-                      255: 'void'}
-        categories = {k + 1: v for k, v in categories.items()}
+        categories = meta_info["category_labels"]
 
         output_generator = KittiMOTSOutputGenerator(output_dir, OnlineChainer.OUTLIER_LABEL, args.save_vis,
                                                     upscaled_inputs=cluster_full_scale,
